d757bd617feab915fc984e5d0a25143ff16ed48a
Julio Guzman <julioegb@gmail.com>
2022-02-08 07:17:31 +0100

    REST API working with two routes: get by name and get all ran bibs

---
+++ ethics4eu/src/main/java/com/ethics4eu/ethics4eu/Ethics4euApplication.java
@@ -0,0 +1,13 @@
+package com.ethics4eu.ethics4eu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ethics4euApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ethics4euApplication.class, args);
+	}
+
+}
